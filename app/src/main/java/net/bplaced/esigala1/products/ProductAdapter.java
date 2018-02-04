722561188de82cf1377f84bc136fc150a8d6e059
esigala1 <sigala.effie@gmail.com>
2018-02-05 00:18:08 +0200

    Revert "Implement OnClickListener to Activity"
    This reverts commit a0d46fe24a4c2c9ae22e82fab051ecc6b86cd62a.

--- app/src/main/java/net/bplaced/esigala1/products/ProductAdapter.java
+++ app/src/main/java/net/bplaced/esigala1/products/ProductAdapter.java
@@ -167,10 +167,9 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
         private final int nameIndex;
         private final int priceIndex;
         private final int quantityIndex;
-
+        // Note: Declare the variable "productID" here to be able to pass it as
+        // a parameter in the "mClickHandler.onClickItem()" method.
         private int productID;
-        private String productName;
-        private int productQTY;
 
         ProductViewHolder(View view) {
             super(view);
@@ -187,9 +186,7 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
             priceIndex = mCursor.getColumnIndexOrThrow(ProductEntry.COLUMN_PRODUCT_PRICE);
             quantityIndex = mCursor.getColumnIndexOrThrow(ProductEntry.COLUMN_PRODUCT_QTY);
 
-            // Set the Listeners.
             view.setOnClickListener(this);
-            saleButton.setOnClickListener(this);
         }
 
         void bind(int listIndex) {
@@ -202,9 +199,9 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
 
             // Read the product attributes from the Cursor for the current product.
             productID = mCursor.getInt(idIndex);
-            productName = mCursor.getString(nameIndex);
-            productQTY = mCursor.getInt(quantityIndex);
+            final String productName = mCursor.getString(nameIndex);
             final double productPrice = mCursor.getDouble(priceIndex);
+            final int productQTY = mCursor.getInt(quantityIndex);
 
             // Get the TextView from the ViewHolder object and set the the product name.
             nameTextView.setText(productName);
@@ -216,6 +213,63 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
 
             // Display the quantity of the product.
             displayQTY(mContext, productQTY);
+
+            // Set the OnClick listener - SALE button.
+            saleButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.v(LOG_TAG, "Sale button clicked => " + productName);
+                    // Variable to put the message to display corresponding to availability.
+                    String message = "";
+                    // If the item is not out of stock, then...
+                    if (productQTY > 0) {
+                        // Create a ContentValues object where column names are the keys,
+                        // and product attributes from the editor are the values.
+                        ContentValues values = new ContentValues();
+                        values.put(ProductEntry.COLUMN_PRODUCT_QTY, productQTY - 1);
+
+                        // Form the content URI that represents the specific product that was clicked on,
+                        // by appending the "id" (passed as input to this method) onto the
+                        // {@link ProductEntry#CONTENT_URI}.
+                        // For example, the URI would be "content://net.bplaced.esigala1.products/products/2"
+                        // if the product with ID 2 was clicked on.
+                        Uri currentProductUri = ContentUris.withAppendedId(ProductEntry.CONTENT_URI, productID);
+
+                        Log.v(LOG_TAG, "URI = " + currentProductUri);
+
+                        // Update the new quantity of the product in the SQLite Database.
+                        int rowsAffected = mContext.getContentResolver().update(currentProductUri, values, null, null);
+
+                        // Show a toast message depending on whether or not the update was successful.
+                        if (rowsAffected == 0) {
+                            // If no rows were affected, then there was an error with the update.
+                            message = mContext.getResources().getString(R.string.editor_update_product_failed);
+                        }
+                        else {
+                            // Display the new quantity of the product.
+                            displayQTY(mContext, productQTY - 1);
+                            // Otherwise, the update was successful and we can display a toast.
+                            message = mContext.getResources().getString(R.string.msg_item_sold, productName);
+                        }
+                    }
+                    else {
+                        message = mContext.getResources().getString(R.string.msg_item_out_of_stock, productName);
+                    }
+
+                    // If the "mToast" object is not null, then cancel it.
+                    if (mToast != null){
+                        mToast.cancel();
+                    }
+                    // Instantiate the Toast object.
+                    mToast = Toast.makeText(
+                            mContext,           // Context
+                            message,            // Message
+                            Toast.LENGTH_SHORT  // Duration
+                    );
+                    // Show the toast message.
+                    mToast.show();
+                }
+            });
         }
 
         /**
@@ -233,99 +287,19 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
             }
         }
 
-        // ***************************************************************************************** //
-        // Implementation for the "View.OnClickListener" interface
-        // ***************************************************************************************** //
-
         /**
-         * This gets called by the child views during a click.
+         * This gets called by the child views during a click. We fetch the date that has been
+         * selected, and then call the onClickItem handler registered with this adapter, passing that
+         * date.
          *
-         * @param view the View that was clicked
+         * @param v the View that was clicked
          */
         @Override
-        public void onClick(View view) {
-            switch (view.getId()){
-                // If the button "SALE" was clicked, then...
-                case R.id.product_sale:
-                    // Sale the product.
-                    saleProduct();
-                    break;
-                // If the above case does not match, then the rest of the space in the row was clicked, so...
-                default:
-                    // Edit the product.
-                    editProduct();
-                    break;
-            }
-        }
-
-        /**
-         * Method to edit a product.
-         */
-        private void editProduct(){
-            Log.d(LOG_TAG, "editProduct()");
-            // Return the Adapter position of the item represented by the ViewHolder,
-            // which is the position of the clicked item.
+        public void onClick(View v) {
+            Log.d(LOG_TAG, "onClickItem()");
             int adapterPosition = getAdapterPosition();
-            // Move the cursor to the position of the clicked item.
             mCursor.moveToPosition(adapterPosition);
-            // Call the onClickItem() method of the custom interface.
             mClickHandler.onClickItem(adapterPosition, productID);
-        }
-
-        /**
-         * Method to sell a product.
-         */
-        private void saleProduct(){
-            Log.d(LOG_TAG, "saleProduct(): Sale button clicked => " + productName);
-            // Variable to put the message to display corresponding to availability.
-            String message = "";
-            // If the item is not out of stock, then...
-            if (productQTY > 0) {
-                // Create a ContentValues object where column names are the keys,
-                // and product attributes from the editor are the values.
-                ContentValues values = new ContentValues();
-                values.put(ProductEntry.COLUMN_PRODUCT_QTY, productQTY - 1);
-
-                // Form the content URI that represents the specific product that was clicked on,
-                // by appending the "id" (passed as input to this method) onto the
-                // {@link ProductEntry#CONTENT_URI}.
-                // For example, the URI would be "content://net.bplaced.esigala1.products/products/2"
-                // if the product with ID 2 was clicked on.
-                Uri currentProductUri = ContentUris.withAppendedId(ProductEntry.CONTENT_URI, productID);
-
-                Log.d(LOG_TAG, "URI = " + currentProductUri);
-
-                // Update the new quantity of the product in the SQLite Database.
-                int rowsAffected = mContext.getContentResolver().update(currentProductUri, values, null, null);
-
-                // Show a toast message depending on whether or not the update was successful.
-                if (rowsAffected == 0) {
-                    // If no rows were affected, then there was an error with the update.
-                    message = mContext.getResources().getString(R.string.editor_update_product_failed);
-                }
-                else {
-                    // Display the new quantity of the product.
-                    displayQTY(mContext, productQTY - 1);
-                    // Otherwise, the update was successful and we can display a toast.
-                    message = mContext.getResources().getString(R.string.msg_item_sold, productName);
-                }
-            }
-            else {
-                message = mContext.getResources().getString(R.string.msg_item_out_of_stock, productName);
-            }
-
-            // If the "mToast" object is not null, then cancel it.
-            if (mToast != null){
-                mToast.cancel();
-            }
-            // Instantiate the Toast object.
-            mToast = Toast.makeText(
-                    mContext,           // Context
-                    message,            // Message
-                    Toast.LENGTH_SHORT  // Duration
-            );
-            // Show the toast message.
-            mToast.show();
         }
     }
 }
